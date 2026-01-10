8ba3fcce8dfa68624c0ddb6d8aeccb383965abe9
Sergei Grichine <msghub@hotmail.com>
2026-01-09 18:32:59 -0600

    Update mpu9250.launch.py

--- launch/mpu9250.launch.py
+++ launch/mpu9250.launch.py
@@ -1,26 +1,51 @@
-import os
-
-from ament_index_python.packages import get_package_share_directory
-
 from launch import LaunchDescription
 from launch_ros.actions import Node
 
 def generate_launch_description():
-    #get_package_share_directory(package_name)
 
     return LaunchDescription([
         Node(
-           package="mpu9250",
-           executable="mpu9250",
-           name="mpu9250",
-             parameters=[
-                {"acceleration_scale": [1.0072387165748442, 1.0081436035838134, 0.9932769089604535], 
-                "acceleration_bias": [0.17038044467587418, 0.20464685207217453, -0.12461014438322202], 
-                "gyro_bias": [0.0069376404996494, -0.0619247665634732, 0.05717760948453845], 
-                "magnetometer_bias": [0.4533159894397744, 3.4555818146055564, -5.984038606178013], 
-                "magnetometer_transform": [   0.9983016121720226, 0.044890057238382707, 0.007231924972024632, 
-                                    0.044890057238382707, 1.2981683205953654, -0.1173361838042438, 
-                                    0.007231924972024633, -0.11733618380424381, 0.7835617468652673]}  
-                ],
+            package="mpu9250",
+            executable="mpu9250",
+            name="mpu9250",
+            parameters=[{
+                "print", False,
+                "frequency", 30,
+                "frame_id", "imu_link",
+                "i2c_address", 0x68,  # also, 0x0C shows up for built-in AK8963 magnetometer
+                "i2c_port", 1,        # a.k.a "bus". For Linux on Raspberry Pi Bus=1
+                "acceleration_scale", [1.0, 1.0, 1.0],
+                "acceleration_bias", [0.0, 0.0, 0.0],
+                "gyro_bias", [0.0, 0.0, 0.0],
+                "magnetometer_scale", [1.0, 1.0, 1.0],
+                "magnetometer_bias", [1.0, 1.0, 1.0],
+                "magnetometer_transform", [
+                    1.0, 0.0, 0.0,
+                    0.0, 1.0, 0.0,
+                    0.0, 0.0, 1.0]
+                #"acceleration_scale": [1.0072387165748442, 1.0081436035838134, 0.9932769089604535],
+                #"acceleration_bias": [0.17038044467587418, 0.20464685207217453, -0.12461014438322202],
+                #"gyro_bias": [0.0069376404996494, -0.0619247665634732, 0.05717760948453845],
+                #"magnetometer_bias": [0.4533159894397744, 3.4555818146055564, -5.984038606178013],
+                #"magnetometer_transform": [   0.9983016121720226, 0.044890057238382707, 0.007231924972024632,
+                #                    0.044890057238382707, 1.2981683205953654, -0.1173361838042438, 
+                #                    0.007231924972024633, -0.11733618380424381, 0.7835617468652673]
+                }],
+        ),
+
+        # for experiments: RViz starts with "map" as Global Fixed Frame, provide a TF to see axes etc.
+        Node(
+            package = "tf2_ros", 
+            executable = "static_transform_publisher",
+            arguments=[
+                '--x', '0.0',     # X translation in meters
+                '--y', '0.0',     # Y translation in meters
+                '--z', '0.1',     # Z translation in meters
+                '--roll', '0.0',  # Roll in radians
+                '--pitch', '0.0', # Pitch in radians
+                '--yaw', '0.0',   # Yaw in radians (e.g., 90 degrees)
+                '--frame-id', 'map', # Parent frame ID
+                '--child-frame-id', 'imu_link' # Child frame ID
+            ]
         )
     ])
