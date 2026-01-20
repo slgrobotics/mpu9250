8cbc9aed388a115764d89dbff0fc52a85e6e3612
Sergei Grichine <msghub@hotmail.com>
2026-01-20 17:58:22 -0600

    WIP: calibration and validating scripts

--- tests/read_mag.py
+++ tests/read_mag.py
@@ -1,4 +1,5 @@
 import time
+import numpy as np
 from smbus2 import SMBus
 
 from mpu9250.imusensor.MPU9250 import MPU9250
@@ -22,46 +23,25 @@ imu.begin()
 
 print("OK: IMU initialized")
 
-# imu.calibrateGyro()
-# imu.calibrateAccelerometer()
-# or load your own calibration file
-#imu.loadCalibDataFromFile("/home/pi/calib_real_bolder.json")
 
-# Note: Make sure you rotate the sensor in 8 shape and cover all the pitch and roll angles.
-
-#imu.calibrateMagPrecise()
-
-print("IP: Reading initial mag values and orientation")
-
-count = 0
-while count < 5:
-    imu.readSensor()
-    imu.computeOrientation()
-
-    print(f"MagVals: {imu.MagVals}     roll:{imu.roll:8.2f}     pitch:{imu.pitch:8.2f}     yaw:{imu.yaw:8.2f}")
-
-    time.sleep(0.2)
-    count += 1
-
-print("calibrating - rotate the sensor in 8 shape and cover all the pitch and roll angles")
-
-#imu.calibrateMagApprox()
-imu.calibrateMagPrecise()
+imu.MagBias = np.array([3.14156455e-05, 6.44644552e-06, 2.43798173e-05])
+imu.Mags = np.array([1., 1., 1.])
+imu.Magtransform = np.array([[ 1.05464768, -0.03982411, -0.01269338],
+                            [-0.03982411,  0.93736574, -0.01455147],
+                            [-0.01269338, -0.01455147,  1.01356064]])
 
 print(f"MagBias: {imu.MagBias}")
 print(f"Mags: {imu.Mags}")
 print(f"Magtransform: {imu.Magtransform}")
 
-print("IP: Reading mag values and orientation after calibration")
+print("IP: Reading mag values and orientation")
 
-count = 0
-while count < 10:
+while True:
     imu.readSensor()
     imu.computeOrientation()
 
     print(f"MagVals: {imu.MagVals}     roll:{imu.roll:8.2f}     pitch:{imu.pitch:8.2f}     yaw:{imu.yaw:8.2f}")
 
     time.sleep(0.2)
-    count += 1
 
 print("Done")
