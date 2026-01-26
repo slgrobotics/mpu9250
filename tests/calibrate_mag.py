3484c139b7b1c2b2e9f5e511c691705292938226
Sergei Grichine <msghub@hotmail.com>
2026-01-25 18:26:30 -0600

    Update calibrate_mag.py

--- tests/calibrate_mag.py
+++ tests/calibrate_mag.py
@@ -72,7 +72,9 @@ def main():
                 print(f"    {row_str}]")
     print()
 
-    read_orientation(imu, 10, "IP: Reading mag values and orientation after calibration")
+    input("\nCalibration complete. Press Enter to see calibrated values...")
+
+    read_orientation(bus, 10000, "IP: Reading mag values and orientation after calibration")
 
     print("Done")
 
